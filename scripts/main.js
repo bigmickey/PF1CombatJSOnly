7509f406c755df892c91ef753749881437d2dc97
Michael <dohertym12@hotmail.com>
2022-02-01 15:03:02 +0400

    generating hit points

--- scripts/main.js
+++ scripts/main.js
@@ -13,5 +13,24 @@ const startCombatBtn = document.getElementById("start_combat_btn");
 startCombatBtn.addEventListener("click", startCombat);
 
 function startCombat() {
-  document.getElementById('combat_results').innerHTML = "here is some text";
+  var combatant1 = new Object();
+  var combatant2 = new Object();
+  var combatResultsDiv = document.getElementById('combat_results');
+  combatResultsDiv.innerHTML += "<p>Starting combat ...</p>";
+  combatResultsDiv.innerHTML += "<p>Generating combatant stats</p>";
+  
+  /* generate stats */
+  /* Goblin */
+  /* hps = 1d10+1 */
+  /* AC 16 */
+  /* Melee short sword +1 1d4/19-20 */
+  combatant1.hps = Math.ceil(Math.random() * 10) + 1;
+  combatResultsDiv.innerHTML += "<p>Goblin hit points = " + combatant1.hps + "</p>";
+
+  /* Orc */
+  /* hps = 1d10+1 */
+  /* AC 13 */
+  /* Melee falchion +4 2d4+4/18-20 */
+  combatant2.hps = Math.ceil(Math.random() * 10) + 1;
+  combatResultsDiv.innerHTML += "<p>Orc hit points = " + combatant2.hps + "</p>";
 }
